a8d9e653f033a5fb5ac8b1c5a2c0fc032864aaa8
fiacredev <fiacredeveloper@gmail.com>
2026-02-18 14:09:50 +0200

    added checking if driver exist and available

--- Services/driverService.js
+++ Services/driverService.js
@@ -3,11 +3,25 @@ import Driver from "../models/Driver.js";
 import Delivery from "../models/Delivery.js";
 
 export const updateLocation = async (driverId, lat, lng) => {
-  return await Driver.findByIdAndUpdate(
-    driverId,
-    { currentLocation: { lat, lng } },
-    { new: true }
-  );
+   const driver = await Driver.findById(driverId);
+  if (!driver) throw new Error("Driver not found");
+  // Check if driver is available or has a delivery in progress being done
+  const activeDelivery = await Delivery.findOne({
+    driver: driver._id,
+    status: "in_progress"
+  });
+
+  if (driver.isAvailable || activeDelivery) {
+    // Update driver current location to be stored effecvtively in database
+    driver.currentLocation = { lat, lng };
+    await driver.save();
+    // Store location history of every drivr
+    await Location.create({ driver: driver._id, lat, lng });
+    return driver;
+  } else {
+    // Skip tracking automatically when all condition aboev are false
+    return driver;
+  }
 };
 
 export const toggleAvailability = async (driverId) => {
