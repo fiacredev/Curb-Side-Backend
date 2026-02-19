1e8ea679aba971f24d44b277fb34690f1ed92982
fiacredev <fiacredeveloper@gmail.com>
2026-02-19 19:24:42 +0200

    first commit in socket file deal with registering driveSockets

---
+++ sockets/driverSocket.js
@@ -0,0 +1,34 @@
+import * as driverService from '../Services/driverService.js'
+
+export const registerDriverSockets = (io) => {
+  io.on('connection', (socket) => {
+    console.log('⚡ Client connected:', socket.id);
+
+    socket.on('driver:location', async (payload) => {
+      try {
+        const { driverId, lat, lng } = payload;
+
+        const driver = await driverService.updateLocationRealtime(
+          driverId,
+          lat,
+          lng
+        );
+
+        // broadcast to all listeners
+        io.emit('driver:update', {
+          driverId,
+          lat,
+          lng,
+        });
+
+        console.log('Location updated:', driverId);
+      } catch (err) {
+        console.error('Socket error:', err.message);
+      }
+    });
+
+    socket.on('disconnect', () =>
+      console.log('Client disconnected:', socket.id)
+    );
+  });
+};
