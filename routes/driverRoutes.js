a983a74cc2d40037cdcdcec754061d69c1e20fe5
fiacredev <fiacredeveloper@gmail.com>
2026-02-19 19:22:06 +0200

    added fetching current location

--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -6,4 +6,7 @@ const router = express.Router();
 router.post('/location', controller.updateLocation);
 router.patch('/:id/toggle', controller.toggleAvailability);
 
+router.get('/:id/location',controller.fetchCurrentLocation );
+
+
 export default router;
