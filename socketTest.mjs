a63b05525fe73a02b95d6e6c93f32c9736c12630
fiacredev <fiacredeveloper@gmail.com>
2026-02-19 19:31:16 +0200

    first commit in socketTest file about handling socket connection of socket

---
+++ socketTest.mjs
@@ -0,0 +1,17 @@
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+
+socket.on('connect', () => {
+  console.log('Connected');
+
+  socket.emit('driver:location', {
+    driverId: '6995a1441287438bcc1b863d',
+    lat: 12.34,
+    lng: 56.78,
+  });
+});
+
+socket.on('driver:update', (data) => {
+  console.log('Broadcast received:', data);
+});
