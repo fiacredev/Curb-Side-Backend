ce8190e5f887c3b1f3077efbac1705418b776e6f
fiacredev <fiacredeveloper@gmail.com>
2026-02-19 19:20:25 +0200

    done about adding fetching current location

--- controllers/driverController.js
+++ controllers/driverController.js
@@ -1,4 +1,5 @@
 import * as driverService from "../Services/driverService.js"
+import Location from "../models/Location.js";
 
 export const updateLocation = async (req, res) => {
   const { driverId, lat, lng } = req.body;
@@ -10,3 +11,11 @@ export const toggleAvailability = async (req, res) => {
   const driver = await driverService.toggleAvailability(req.params.id);
   res.json(driver);
 };
+
+export const fetchCurrentLocation = async (req,res) =>{
+  const location = await Location
+    .findOne({ driver: req.params.id })
+    .sort({ timestamp: -1 });
+
+  res.json(location);
+}
