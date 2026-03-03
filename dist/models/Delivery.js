4418a8574ae83c2604f1b4832fd99c49d7bba5a4
fiacredev <fiacredeveloper@gmail.com>
2026-03-03 18:05:41 +0200

    fields changed to default null

--- dist/models/Delivery.js
+++ dist/models/Delivery.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 const deliverySchema = new Schema({
-    customer: { type: Schema.Types.ObjectId, ref: "Customer", required: true },
-    driver: { type: Schema.Types.ObjectId, ref: "Driver", required: true },
+    customer: { type: Schema.Types.ObjectId, ref: "Customer", required: true, default: null },
+    driver: { type: Schema.Types.ObjectId, ref: "Driver", required: true, default: null },
     pickup: {
         lat: { type: Number, required: true },
         lng: { type: Number, required: true },
