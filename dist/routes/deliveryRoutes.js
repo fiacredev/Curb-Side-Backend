5721b6607999d39790c2d090375b78516360d49e
fiacredev <fiacredeveloper@gmail.com>
2026-03-05 23:11:09 +0200

    get nearest drivers

--- dist/routes/deliveryRoutes.js
+++ dist/routes/deliveryRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import * as controller from '../controllers/deliveryController.js';
 const router = express.Router();
-router.get("/:customerId", controller.getCustomerDeliveries);
 router.get("/nearby", controller.getNearbyDeliveries);
+router.get("/:customerId", controller.getCustomerDeliveries);
 router.post('/', controller.createDelivery);
 router.patch('/:id/status', controller.updateStatus);
 export default router;
