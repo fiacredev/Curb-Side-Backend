1957af53406fc3b44ac308fba6ea0d95d18f7ddd
fiacredev <fiacredeveloper@gmail.com>
2026-03-05 14:52:10 +0200

    transporter updated

--- utils/mailer.ts
+++ utils/mailer.ts
@@ -1,14 +1,22 @@
 import nodemailer from "nodemailer";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true only for port 465
   auth: {
-    user: process.env.EMAIL_USER as string,
-    pass: process.env.EMAIL_PASS as string,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
-
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.error("SMTP connection error:", error);
+//   } else {
+//     console.log("SMTP server is ready to send emails", success);
+//   }
+// });
 export default transporter;
